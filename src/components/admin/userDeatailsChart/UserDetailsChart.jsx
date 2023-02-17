a57836ebf8b4d91c5716e2b2c12a649b56e341d1
najumudheen <najumudheenck3@gmail.com>
2023-02-17 16:44:09 +0530

    after completing user and post details graph

---
+++ src/components/admin/userDeatailsChart/UserDetailsChart.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+
+const UserDetailsChart = ({userGraphCategories,userGraphData}) => {
+    console.log(userGraphCategories,'userGraphCategoriesuserGraphCategories');
+  const [state, setState] = useState({
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: userGraphCategories?userGraphCategories:"",
+      },
+    },
+    series: [
+      {
+        name: "series-1",
+        data: userGraphData?userGraphData:"",
+      },
+    ],
+  });
+  return (
+    <div className="flex flex-col w-full  content-center justify-center p-5 gap-y-5">
+      <h1 className="text-center text-2xl font-bold">User Details </h1>
+      <div className="mx-auto">
+      <Chart 
+        options={state.options}
+        series={state.series}
+        type="bar"
+        width="600"
+      />
+      </div>
+    </div>
+  );
+};
+
+export default UserDetailsChart;
--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -2,34 +2,62 @@ import React, { useEffect, useState } from "react";
 import GroupIcon from "@mui/icons-material/Group";
 import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import { fetDashboardDetails } from "../../api/admin/UserRequest";
+import UserDetailsChart from "../../components/admin/userDeatailsChart/UserDetailsChart";
+import PostDetailChart from "../../components/admin/postDetailsChart/PostDetailChart";
 
 const Home = () => {
   const [totalUsers,setTotalUsers]=useState('')
   const [activeUsers,setActiveUsers]=useState('')
+  const [userGraphCategories,setUserGraphCategories]=useState([])
+  const [userGraphData,setUserGraphData]=useState([])
+  const [postGraphCategories,setPostGraphCategories]=useState([])
+  const [postGraphData,setPostGraphData]=useState([])
   useEffect(()=>{
 const getDeatil=async()=>{
   const response=await fetDashboardDetails()
   console.log(response,'resssssss')
   setTotalUsers(response?.userCount)
   setActiveUsers(response?.activeCount)
+  // user detail graph data and category setting
+  const usergpcatgry=response?.userGraph.map(item => {
+    return item._id
+  })
+  setUserGraphCategories(usergpcatgry)
+  const usergpdata=response?.userGraph.map(items=>{
+    return items.count
+  })
+  setUserGraphData(usergpdata)
+  // post detail graph data and category setting
+  const postgpcatgry=response?.postGraph.map(item => {
+    return item._id
+  })
+  setPostGraphCategories(postgpcatgry)
+  const postgpdata=response?.postGraph.map(items=>{
+    return items.count
+  })
+  setPostGraphData(postgpdata)
 }
 getDeatil()
   },[])
   return (
     <>
-      <div className="flex mt-5 p-5 w-full text-center gap-x-10 justify-center ">
-        <div className="h-56 w-56 bg-white flex items-center flex-col gap-y-5 rounded-xl p-5">
+     <div className="flex flex-col w-full ">
+     <div className="flex mt-5 p-5 w-full text-center gap-x-10 justify-center ">
+        <div className="h-48 w-48 bg-white flex items-center flex-col gap-y-5 rounded-xl p-5">
           <h1 className="font-bold text-2xl"> Total Users</h1>
           <GroupIcon className="text-specclr" fontSize="large" />
           <h1 className="font-bold text-6xl">{totalUsers}</h1>
 
         </div>
-        <div className="h-56 w-56 bg-white flex items-center flex-col gap-y-5 rounded-xl p-5">
+        <div className="h-48 w-48 bg-white flex items-center flex-col gap-y-5 rounded-xl p-5">
           <h1 className="font-bold text-2xl">Active Users</h1>
           <GroupAddIcon className="text-specclr" fontSize="large" />
           <h1 className="font-bold text-6xl">{activeUsers}</h1>
         </div>
       </div>
+      {userGraphCategories?.length>0&&<UserDetailsChart userGraphCategories={userGraphCategories} userGraphData={userGraphData}/>}
+      {postGraphCategories?.length>0&&<PostDetailChart postGraphCategories={postGraphCategories} postGraphData={postGraphData}/>}
+     </div>
     </>
   );
 };
